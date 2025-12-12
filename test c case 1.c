5c6176fdf1f5de727c49957673aadfa756cb13c3
L1542 <natchanon.pet@ku.th>
2025-12-12 13:29:47 +0700

    Combine case statements for currency conversion

--- test c case 1.c
+++ test c case 1.c
@@ -12,16 +12,13 @@ int main()
     printf("Enter amount in That Baht:");
     scanf("%f",&money);
     switch(answer){
-        case 'D':
-        case 'd':
+        case 'D': case 'd':
         bath = money/33.46 ;
         printf("%.2f bath = %.2f us Dollar",money,bath);break;
-        case 'Y':
-        case 'y':
+        case 'Y': case 'y':
         bath = money/0.29 ;
         printf("%.2f bath = %.2f Japanese Yen",money,bath);break;
-        case 'K':
-        case 'k':
+        case 'K': case 'k':
         bath = money/35.28;
         printf("%.2f bath = %.2f Korean Won",money,bath);break;
     }
