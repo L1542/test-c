3bed614aedf4d015ac7b63c73ffef3b6811c5e69
L1542 <natchanon.pet@ku.th>
2025-12-12 13:28:31 +0700

    Update test c case 1.c

--- test c case 1.c
+++ test c case 1.c
@@ -11,13 +11,18 @@ int main()
     answer = getchar();
     printf("Enter amount in That Baht:");
     scanf("%f",&money);
-    switch(answer)
-    {
-        case 'D': bath = money/33.46 ;
+    switch(answer){
+        case 'D':
+        case 'd':
+        bath = money/33.46 ;
         printf("%.2f bath = %.2f us Dollar",money,bath);break;
-        case 'Y': bath = money/0.29 ;
+        case 'Y':
+        case 'y':
+        bath = money/0.29 ;
         printf("%.2f bath = %.2f Japanese Yen",money,bath);break;
-        case 'K': bath = money/35.28;
+        case 'K':
+        case 'k':
+        bath = money/35.28;
         printf("%.2f bath = %.2f Korean Won",money,bath);break;
     }
     
